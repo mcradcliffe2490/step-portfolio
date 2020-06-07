c00fb4faf1168282970e03f7afbb216ccd78aac4
Mycale Radcliffe <mcradcliffe2490@gmail.com>
2020-06-07 19:52:00 +0000

    Adds async function to script.js so that the portfolio website can fetch data from the servlet

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -18,49 +18,49 @@
 function addRandomQuote() {
   const quotes =
       [ "If you're the CEO of a company, and you're dumb enough" +
-        "to leave your log-in info on a Post-It note on your desk, while the people" +
-        "that you ripped off are physically in your office, it's not a hack. It's" +
-        "barely social engineering. It’s more like natural selection.",
+        " to leave your log-in info on a Post-It note on your desk, while the people" +
+        " that you ripped off are physically in your office, it's not a hack. It's" +
+        " barely social engineering. It’s more like natural selection.",
 
         "If the rise of an all-powerful intelligence is inevitable, well it stands to" +
-        "reason that when they take power, our digital overlords will punish those of us" +
-        "who did not help them get there. Ergo, I would like to be a helpful idiot." +
-        "Like yourself.",
+        " reason that when they take power, our digital overlords will punish those of us" +
+        " who did not help them get there. Ergo, I would like to be a helpful idiot." +
+        " Like yourself.",
 
         "War does not determine who is right- only who is left",
         
         "Nothing's perfect, the world's not perfect, but it's there for us, trying" +
-        "the best it can. That's what makes it so damn beautiful.",
+        " the best it can. That's what makes it so damn beautiful.",
 
         "Before I leaped, I should have seen the view from halfway down", 
 
         "When you look at someone through rose-colored glasses," +
-        "all the red flags just look like flags ",
+        " all the red flags just look like flags ",
 
         "Sometimes I'll start a sentence and I don't even know where it's going." +
-        "I just hope I find it along the way",
+        " I just hope I find it along the way",
 
         "Mini cupcakes? As in the mini version of regular cupcakes?" + 
-        "Which is already a mini version of cake?" + 
-        "Honestly, where does it end with you people?",
+        " Which is already a mini version of cake?" + 
+        " Honestly, where does it end with you people?",
 
         "Whenever I'm about to do something, I think," +
-        "'Would an idiot do that?' and if they would, I do not do that thing",
+        " 'Would an idiot do that?' and if they would, I do not do that thing",
 
         "The doctor said, if I can't find a new way to relate more positively" +
-        "to my surroundings, I'm going to die. I'm going to die",
+        " to my surroundings, I'm going to die. I'm going to die",
 
         "I'm not superstitious, but I am a little stitious.",
 
         "Pirates are evil? The Marines are righteous?... Justice will prevail, you say?" + 
-        "But of course it will! Whoever wins this war becomes justice!",
+        " But of course it will! Whoever wins this war becomes justice!",
 
         "When do you think people die? When they are shot through the heart by the bullet" +
-        "of a pistol? No. When they are ravaged by an incurable disease? No..." +
-        "It’s when they're forgotten!",
+        " of a pistol? No. When they are ravaged by an incurable disease? No..." +
+        " It’s when they're forgotten!",
 
         "Stop counting only those things you have lost! What is gone, is gone!" + 
-        "So ask yourself this. What is there... that still remains to you?!",
+        " So ask yourself this. What is there... that still remains to you?!",
         ];
 
   // Pick a random quote.
@@ -69,4 +69,15 @@ function addRandomQuote() {
   // Add it to the page.
   const quoteContainer = document.getElementById('quote-container');
   quoteContainer.innerText = quote;
+
 }
+
+
+
+  async function getServerResponse() {
+      const response = await fetch("/data");
+      const statement = await response.text();
+      const statementContainer = document.getElementById("statement-container");
+      statementContainer.innerText = statement; 
+
+  }
