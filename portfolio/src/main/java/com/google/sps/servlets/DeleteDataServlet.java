d86f1ab86707845b8f63170a792d66bf62a5a012
Mycale Radcliffe <mcradcliffe2490@gmail.com>
2020-06-22 17:36:57 +0000

    Makes minor style changes

--- portfolio/src/main/java/com/google/sps/servlets/DeleteDataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteDataServlet.java
@@ -11,10 +11,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.List;
-import java.util.ArrayList;
-import com.google.gson.Gson;
-import java.util.Optional;
 
 @WebServlet("/delete-data")
 public class DeleteDataServlet extends HttpServlet {
