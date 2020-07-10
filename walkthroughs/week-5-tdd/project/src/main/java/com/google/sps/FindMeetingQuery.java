2d7254bc1f4353a207fb4b500113327a866fc0f4
Mycale Radcliffe <mcradcliffe2490@gmail.com>
2020-07-10 17:59:18 +0000

    Addresses comments to improve style and readability

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -22,73 +22,73 @@ import java.util.List;
 
 public final class FindMeetingQuery {
 
-    private void addTimeRange(List<TimeRange> timeList, int startTime, int endTime, boolean includeEnd) {
-        timeList.add((TimeRange.fromStartEnd(  
+    public Collection < TimeRange > query(Collection < Event > events, MeetingRequest request) {
+        List < TimeRange > eventTimeRanges = new ArrayList < > ();
+        List < TimeRange > openTimeRanges = new ArrayList < > ();
+
+        if (request.getAttendees() == null || request.getAttendees().isEmpty()) {
+            return Arrays.asList(TimeRange.WHOLE_DAY);
+        }
+
+        if (request.getDuration() > TimeRange.WHOLE_DAY.duration()) {
+            return Arrays.asList();
+        }
+
+        for (Event loopEvents: events) {
+            eventTimeRanges.add((loopEvents.getWhen()));
+        }
+
+        Collections.sort(eventTimeRanges, TimeRange.ORDER_BY_START);
+
+        for (int i = 0; i < eventTimeRanges.size(); i++) {
+            if (i == 0) {
+                addTimeRangeEndExcluded(
+                    openTimeRanges,
+                    TimeRange.START_OF_DAY,
+                    eventTimeRanges.get(i).start()
+                );
+                if (eventTimeRanges.size() > 1) {
+                    addTimeRangeEndExcluded(
+                        openTimeRanges,
+                        eventTimeRanges.get(i).end(),
+                        eventTimeRanges.get(i + 1).start()
+                    );
+                } else {
+                    addTimeRangeEndIncluded(
+                        openTimeRanges,
+                        eventTimeRanges.get(i).end(),
+                        TimeRange.END_OF_DAY
+                    );
+                }
+            } else if (i == eventTimeRanges.size() - 1) {
+                addTimeRangeEndIncluded(
+                    openTimeRanges,
+                    eventTimeRanges.get(i).end(),
+                    TimeRange.END_OF_DAY
+                );
+            } else {
+                addTimeRangeEndExcluded(
+                    openTimeRanges,
+                    eventTimeRanges.get(i).end(),
+                    eventTimeRanges.get(i + 1).start()
+                );
+            }
+        }
+        return openTimeRanges;
+    }
+
+    private void addTimeRange(List < TimeRange > timeList, int startTime, int endTime, boolean includeEnd) {
+        timeList.add((TimeRange.fromStartEnd(
             startTime,
             endTime,
             includeEnd)));
     }
 
+    private void addTimeRangeEndIncluded(List < TimeRange > timeList, int startTime, int endTime) {
+        addTimeRange(timeList, startTime, endTime, true);
+    }
 
-    public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-        List<TimeRange> eventTimeRanges = new ArrayList<>();
-        List<TimeRange> openTimeRanges = new ArrayList<>();
-
-        if(request.getAttendees() != null && !request.getAttendees().isEmpty()) {
-            if(TimeRange.WHOLE_DAY.duration() >= request.getDuration()) {
-                for(Event loopEvents: events) {
-                    eventTimeRanges.add((loopEvents.getWhen()));
-                }
-
-                Collections.sort(eventTimeRanges, TimeRange.ORDER_BY_START);
-
-                for(int i = 0; i < eventTimeRanges.size(); i++) {
-                    if (i == 0) {
-                        addTimeRange(
-                            openTimeRanges,
-                            TimeRange.START_OF_DAY,
-                            eventTimeRanges.get(i).start(),
-                            false
-                        );
-                            if (eventTimeRanges.size() > 1) {
-                                addTimeRange(
-                                    openTimeRanges,
-                                    eventTimeRanges.get(i).end(),
-                                    eventTimeRanges.get(i+1).start(),
-                                    false
-                                );
-                            } else {
-                                addTimeRange(
-                                    openTimeRanges,
-                                    eventTimeRanges.get(i).end(),
-                                    TimeRange.END_OF_DAY,
-                                    true
-                                );
-                            }
-                    } else if (i == eventTimeRanges.size()-1) {
-                        addTimeRange(
-                            openTimeRanges,
-                            eventTimeRanges.get(i).end(),
-                            TimeRange.END_OF_DAY,
-                            true
-                        );
-                    } else {
-                        addTimeRange(
-                            openTimeRanges,
-                            eventTimeRanges.get(i).end(),
-                            eventTimeRanges.get(i+1).start(),
-                            false
-                        );
-                    }
-                }
-                return openTimeRanges;
-
-            } else {
-                return Arrays.asList();
-            }
-
-        } else {
-            return Arrays.asList(TimeRange.WHOLE_DAY);
-        }
+    private void addTimeRangeEndExcluded(List < TimeRange > timeList, int startTime, int endTime) {
+        addTimeRange(timeList, startTime, endTime, false);
     }
 }
